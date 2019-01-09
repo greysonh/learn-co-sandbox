82e0f672c46fd7bb96447b15ba000faaa06240cb
Greyson Harris <gharris@esri.com>
2019-01-09 23:50:54 +0000

    Automatically backed up by Learn

--- iss.js
+++ iss.js
@@ -1,5 +1,3 @@
-function getIss() {
-  fetch("http://api.open-notify.org/astros.json")
+fetch("http://api.open-notify.org/astros.json")
   .then(response => response.json())
   .then(json => console.log(json));
-}
